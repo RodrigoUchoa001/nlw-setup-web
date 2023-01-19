9a310210c6e6208ce6e1f1b1bba284fa3d4d6247
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-01-18 22:50:11 -0300

    aula 2- criado componente de hábito diário

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,3 +1,5 @@
+import { HabitDay } from "./HabitDay"
+
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
  
 export function SummaryTable(){
@@ -13,9 +15,11 @@ export function SummaryTable(){
                             {weekDay}
                         </div>
                     )
-                })}
+                })}                
+            </div>
 
-                
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
+                <HabitDay />
             </div>
 
         </div>
